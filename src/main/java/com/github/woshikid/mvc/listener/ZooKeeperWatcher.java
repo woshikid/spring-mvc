1ef3fc7997936aa933b8e58b5ebacfd0756de2c2
woshikid <name.kid@gmail.com>
2019-10-15 15:44:08 +0800

    增加java-utils依赖

--- src/main/java/com/github/woshikid/mvc/listener/ZooKeeperWatcher.java
+++ src/main/java/com/github/woshikid/mvc/listener/ZooKeeperWatcher.java
@@ -2,10 +2,11 @@ package com.github.woshikid.mvc.listener;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.ZooKeeper;
+import org.springframework.beans.factory.InitializingBean;
 
 import com.github.woshikid.utils.ChildrenWatcher;
 
-public class ZooKeeperWatcher extends ChildrenWatcher {
+public class ZooKeeperWatcher extends ChildrenWatcher implements InitializingBean {
 
 	public ZooKeeperWatcher(ZooKeeper zk, String path) {
 		super(zk, path);
@@ -13,6 +14,11 @@ public class ZooKeeperWatcher extends ChildrenWatcher {
 
 	protected void doProcess(WatchedEvent event) throws Exception {
 		System.err.println(event);
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		register();
 	}
 
 }
